e74b54fd9575557256bf8f94a4bd3946b4e3532c
TheTBO <taba1950@gmail.com>
2018-08-02 15:54:54 +0800

    still palying around

--- code.js
+++ code.js
@@ -1,20 +1,12 @@
 console.log("its on!!!");
 
-const recognition = new webkitSpeechRecognition();
+let recognition = new webkitSpeechRecognition();
 
 recognition.onResult = function (event) {
     console.log("Result Bruh!");
     console.log(event);
 };
 
-recognition.onError = function (event) {
-    console.log("Error Bruh!");
-    console.log(event);
-};
-
-recognition.onStart = function (event) {
-    console.log("Started Bruh!");
-    console.log(event);
-};
-
 recognition.start();
+
+console.log("Nothing?");
